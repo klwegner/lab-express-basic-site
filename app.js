7e6d48f79732e0647a687172659cad5edf32e555
Kristen Wegner <klwegner@gmail.com>
2022-06-29 14:48:04 -0400

    done

---
+++ app.js
@@ -0,0 +1,17 @@
+const { response } = require('express');
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get('/home', (request, response, next) => response.sendFile(__dirname + '/views/home.html'));
+
+app.get('/about', (request, response, next) => response.sendFile(__dirname + '/views/about.html'));
+
+app.get('/photogallery', (request, response, next) => response.sendFile(__dirname + '/views/photogallery.html'));
+
+app.get('/works', (request, response, next) => response.sendFile(__dirname + '/views/works.html'));
+
+
+app.listen(3000, () => console.log('Find the Truth! '));
+
